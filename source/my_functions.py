345a8a7c712a6863c9b2bd14ee753609eefaf786
doggbmx <manuelorticosta@gmail.com>
2024-02-14 11:14:01 -0300

    Add error handling for division by zero

--- source/my_functions.py
+++ source/my_functions.py
@@ -3,4 +3,6 @@ def add(number_one, number_two):
     return number_one + number_two
 
 def divide(number_one, number_two):
+    if number_two == 0:
+        raise ValueError("You can't divide by zero!")
     return number_one / number_two
