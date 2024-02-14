0a2670355fd067cff9d90602f7ff7d9fa52e7944
doggbmx <manuelorticosta@gmail.com>
2024-02-14 11:44:12 -0300

    Add imports and new tests to test_my_functions.py

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 import source.my_functions as my_functions
 
 def test_add():
@@ -16,7 +17,24 @@ def test_divide():
 ## BONUS TRACK
 def test_divide_by_zero():
     ## Con esto aseguramos que de la funcion tire un error de ZeroDivisionError
-    with pytest.raises(ZeroDivisionError):
+    with pytest.raises(ValueError):
         ## Es decir, el test pasa porque sabemos que tira un error de ZeroDivisionError
         my_functions.divide(1, 0)
+
+
+## TIME ISSUES -- will only run if you run `pytest -m slow`
+@pytest.mark.slow  
+def test_very_slow_function():
+    time.sleep(1)
+    result = my_functions.divide(10, 5)
+    assert result == 2      
+        
+@pytest.mark.skip(reason="This feature is currently broken")
+def test_add():
+    ## This would be the output: `s....`
+    assert my_functions.add(1, 2) == 3
     
+@pytest.mark.xfail(reason="We know we can not divide by zero")
+def test_divide_by_zero_broken():
+    ## This would be the output: `s....x`
+    assert my_functions.divide(1, 0)
